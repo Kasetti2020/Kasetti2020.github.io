01b2092d1d7e42d90c91f77baa3e1bf880c22da2
sharan018 <115627304+sharan018@users.noreply.github.com>
2023-07-07 15:03:06 +0530

    reactjs

---
+++ react-in-salesforce/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Components from './Components';
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>hello worldss</h1>
+       <Components/>
+      </header>
+    </div>
+  );
+}
+
+
